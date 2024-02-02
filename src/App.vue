e9332914ce1fbd624007829914b4c463300d37ff
Warley Rodrigues <warleyhacker@hotmail.com>
2024-02-02 03:04:59 -0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script lang="ts">
 
 import { fromLonLat } from 'ol/proj';
+import { Vector, Layer } from 'ol/layer';
+import SourceVector from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import * as Style from 'ol/style';
+import { Circle } from 'ol/geom';
 import Map from 'ol/Map';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
@@ -9,19 +14,12 @@ import View from 'ol/View.js';
 export default {
 
   data() {
-
-
     return {
-
-      map: new Map,
-
-
-
+      map: new Map
     }
   },
 
   mounted() {
-
     this.map.setTarget('map');
 
     this.map.setLayers([
@@ -33,17 +31,59 @@ export default {
     this.map.setView(new View({
       center: fromLonLat([0, 0]),
       zoom: 0
-    }));    
+    }));
+
+    var centro = fromLonLat([-44.0316416, -20.0269543]);
 
     setTimeout(() => {
-      var view = this.map.getView();
-      view.setCenterInternal(fromLonLat([-44.0316416, -20.0269543]));
+      let view = this.map.getView();
+      view.setCenterInternal(centro);
       view.setZoom(18);
+
+      // add circle
+      let circle = new Circle(centro, 100);
+
+      let layer_circle = new Vector({
+        source: new SourceVector({
+          features: [new Feature(circle)]
+        }),
+
+        style: new Style.Style({
+          stroke: new Style.Stroke({
+            color: 'red',
+            width: 2
+          }),
+
+          fill: new Style.Fill({
+            color: 'rgba(255, 0, 0, 0.1)'
+          })
+        })
+      })
+
+      this.map.addLayer(layer_circle);
+
+      // remove circle
+      setTimeout(() => {
+        layer_circle.setSource(null);
+        layer_circle.setStyle(null);
+      }, 3000)
+
+      // not found
+      // this.map.removeLayer(layes_circle)
+      //  this.map.getLayers().forEach(vl => {      
+      //     // if (layer.get('name') && layer.get('name') == 'flag_vectorLayer') {
+      //       this.map.removeLayer(vl)
+      //     // }
+      //   });
     }, 1000)
 
 
 
-    
+
+
+    // https://openlayers.org/en/latest/examples/device-orientation.html
+
+
   },
 }
 </script>
